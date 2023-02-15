e863dcd9db88b1ae95d490d19ddec540bb558293
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-02-15 16:06:20 +0100

    adds newUsers adding

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,12 +14,29 @@ const socket = io("http://localhost:3001", { transports: ["websocket"] });
 const Home = () => {
   const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([]);
 
   useEffect(() => {
     socket.on("welcome", (welcomeMessage) => {
       console.log(welcomeMessage);
+
+      socket.on("loggedIn", (onlineUsersList) => {
+        console.log("logged in event: ", onlineUsersList);
+        setLoggedIn(true);
+        setOnlineUsers(onlineUsersList);
+      });
+
+      socket.on("updateOnlineUsersList", (onlineUsersList) => {
+        console.log("A new user connected/disconnected");
+        setOnlineUsers(onlineUsersList);
+      });
     });
   });
+
+  const submitUsername = () => {
+    socket.emit("setUsername", { username });
+  };
 
   return (
     <Container fluid>
@@ -31,12 +48,14 @@ const Home = () => {
           <Form
             onSubmit={(e) => {
               e.preventDefault();
+              submitUsername();
             }}
           >
             <FormControl
               placeholder="Set your username here"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
+              disabled={loggedIn}
             />
           </Form>
           {/* )} */}
@@ -52,12 +71,23 @@ const Home = () => {
               placeholder="Write your message here"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
+              disabled={!loggedIn}
             />
           </Form>
         </Col>
         <Col md={3}>
           {/* ONLINE USERS SECTION */}
           <div className="mb-3">Connected users:</div>
+          {onlineUsers.length === 0 && (
+            <ListGroup.Item>Log in to check who is online!!</ListGroup.Item>
+          )}
+          <ListGroup>
+            {onlineUsers.map((user) => (
+              <ListGroup.Item key={user.socketId}>
+                {user.username}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
         </Col>
       </Row>
     </Container>
