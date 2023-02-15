4a5fed0b19b4121b67bebd15a61094f626cf82bc
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-02-15 15:26:13 +0100

    minor changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import {
   Container,
   Row,
@@ -6,12 +6,16 @@ import {
   Form,
   FormControl,
   ListGroup,
-  Button,
-} from "react-bootstrap"
+} from "react-bootstrap";
+import { Message, User } from "../types";
+import { io } from "socket.io-client";
 
+const socket = io("http://localhost:3001", { transports: ["websocket"] });
 const Home = () => {
-  const [username, setUsername] = useState("")
-  const [message, setMessage] = useState("")
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {});
 
   return (
     <Container fluid>
@@ -21,14 +25,14 @@ const Home = () => {
           {/* TOP AREA: USERNAME INPUT FIELD */}
           {/* {!loggedIn && ( */}
           <Form
-            onSubmit={e => {
-              e.preventDefault()
+            onSubmit={(e) => {
+              e.preventDefault();
             }}
           >
             <FormControl
               placeholder="Set your username here"
               value={username}
-              onChange={e => setUsername(e.target.value)}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </Form>
           {/* )} */}
@@ -36,14 +40,14 @@ const Home = () => {
           <ListGroup></ListGroup>
           {/* BOTTOM AREA: NEW MESSAGE */}
           <Form
-            onSubmit={e => {
-              e.preventDefault()
+            onSubmit={(e) => {
+              e.preventDefault();
             }}
           >
             <FormControl
               placeholder="Write your message here"
               value={message}
-              onChange={e => setMessage(e.target.value)}
+              onChange={(e) => setMessage(e.target.value)}
             />
           </Form>
         </Col>
@@ -53,7 +57,7 @@ const Home = () => {
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
